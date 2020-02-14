83cd295356ff9fe3b8f238015e5e15c2238701da
NatZha <50129222+NatZha@users.noreply.github.com>
2020-02-14 22:28:53 +1100

    new

---
+++ Makefile
@@ -0,0 +1,2 @@
+main: main.c buffer.c buffer.h datatypes.h bldc_interface.c bldc_interface.h
+	gcc -o main main.c buffer.c buffer.h datatypes.h bldc_interface.c bldc_interface.h
---
+++ main.c
@@ -0,0 +1,174 @@
+// #include <stdio.h>
+
+// int main(void) {
+//     printf("Hello WOrld");
+// }
+
+#include <errno.h>
+#include <fcntl.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+
+#include "buffer.h"
+#include "datatypes.h"
+#include "bldc_interface.h"
+
+static mc_values values;
+static mc_configuration mcconf;
+static app_configuration appconf;
+
+
+////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////
+
+
+
+int set_interface_attribs(int fd, int speed)
+{
+    struct termios tty;
+
+    if (tcgetattr(fd, &tty) < 0) {
+        printf("Error from tcgetattr: %s\n", strerror(errno));
+        return -1;
+    }
+
+    cfsetospeed(&tty, (speed_t)speed);
+    cfsetispeed(&tty, (speed_t)speed);
+
+    tty.c_cflag |= (CLOCAL | CREAD);    /* ignore modem controls */
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8;         /* 8-bit characters */
+    tty.c_cflag &= ~PARENB;     /* no parity bit */
+    tty.c_cflag &= ~CSTOPB;     /* only need 1 stop bit */
+    tty.c_cflag &= ~CRTSCTS;    /* no hardware flowcontrol */
+
+    /* setup for non-canonical mode */
+    tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+    tty.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+    tty.c_oflag &= ~OPOST;
+
+    /* fetch bytes as they become available */
+    tty.c_cc[VMIN] = 1;
+    tty.c_cc[VTIME] = 1;
+
+    if (tcsetattr(fd, TCSANOW, &tty) != 0) {
+        printf("Error from tcsetattr: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+void set_mincount(int fd, int mcount)
+{
+    struct termios tty;
+
+    if (tcgetattr(fd, &tty) < 0) {
+        printf("Error tcgetattr: %s\n", strerror(errno));
+        return;
+    }
+
+    tty.c_cc[VMIN] = mcount ? 1 : 0;
+    tty.c_cc[VTIME] = 5;        /* half second timer */
+
+    if (tcsetattr(fd, TCSANOW, &tty) < 0)
+        printf("Error tcsetattr: %s\n", strerror(errno));
+}
+
+
+// void interpret_serial(unsigned char *, ){
+
+// }
+
+
+
+
+int main()
+{
+    char *portname = "/dev/ttyUSB0";
+    int fd;
+    int wlen;
+
+    fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
+    if (fd < 0) {
+        printf("Error opening %s: %s\n", portname, strerror(errno));
+        return -1;
+    }
+    /*baudrate 115200, 8 bits, no parity, 1 stop bit */
+    set_interface_attribs(fd, B115200);
+    //set_mincount(fd, 0);                /* set to pure timed read */
+
+    /* simple output */
+    wlen = write(fd, "Hello!\n", 7);
+    if (wlen != 7) {
+        printf("Error from write: %d, %d\n", wlen, errno);
+    }
+    tcdrain(fd);    /* delay for output */
+
+
+    /* simple noncanonical input */
+    do {
+        printf("Calling Function: ");
+        bldc_interface_get_values();
+        COMM_GET_VALUES;
+        COMM_PRINT;
+        printf("%f\t", values.current_in);
+        printf("%f\t", values.temp_motor);
+        printf("ENd of call\n");
+
+        
+        
+        for (int c = 1; c <= 100; c++){
+            for (int d = 1; d <= 500; d++)
+            {   
+                printf("Set to 3000\n");
+                bldc_interface_set_rpm(3000);
+            }
+        }
+        for (int c = 1; c <= 100; c++){
+            for (int d = 1; d <= 500; d++)
+            {   
+                printf("Set to 5000\n");
+                bldc_interface_set_rpm(5000);
+            }
+        }
+
+        printf("Exit RPM loops\n");
+
+
+        unsigned char buf[80];
+        int rdlen;
+
+        rdlen = read(fd, buf, sizeof(buf) - 1);
+        if (rdlen > 0) {
+#ifdef DISPLAY_STRING
+            buf[rdlen] = 0;
+            printf("Read %d: \"%s\"\n", rdlen, buf);
+#else /* display hex */
+            unsigned char   *p;
+            //printf("Read %d:", rdlen);
+            for (p = buf; rdlen-- > 0; p++);
+                //printf(" 0x%x", *p);
+            //printf("\n");
+            printf("%s\n", p);
+            
+#endif
+        } else if (rdlen < 0) {
+            printf("Error from read: %d: %s\n", rdlen, strerror(errno));
+        } else {  /* rdlen == 0 */
+            printf("Timeout from read\n");
+        }               
+        /* repeat read to get full message */
+    } while (1);
+}
+
+
